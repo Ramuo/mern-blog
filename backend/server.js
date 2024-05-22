478eb7af98e8f20d3884ee08ec357e49326ea9af
Barry <barry.alpha88@outlook.fr>
2024-05-22 19:41:57 +0200

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -47,21 +47,40 @@ app.use('/api/comments', commentRoute);
 
 
 //STATIC ROUTE
-if(process.env.NODE_ENV === 'production'){
-  const __dirname = path.resolve(); //Set __dir to current directory
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+// const __dirname = path.resolve(); //Set __dir to current directory
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
+// if(process.env.NODE_ENV === 'production'){
+//   const __dirname = path.resolve(); //Set __dir to current directory
+//   app.use('/uploads', express.static('/var/data/uploads'));
+//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+//   );
+// }else{
+//   const __dirname = path.resolve();
+//   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+//   app.get('/', (req, res) => {
+//     res.send('API is running....');
+//   });
+// }
+
+//STATIC ROUTE
+const __dirname = path.resolve(); //Set __dir to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    //any route that is not api will redirect to index.html
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
 }else{
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+    app.get('/', (req, res) => res.send('API Running'));
+};
 
 
 //MIDDLEWARE
